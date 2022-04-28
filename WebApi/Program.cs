a147eefef3233915aa0e78e4f9feb036b6a46709
Claes Engelin <claes.engelin@zocom.se>
2022-04-28 09:31:47 +0200

    Completed DbContext, added API Versioning

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,3 +1,5 @@
+using Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,17 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// This uses the method found in DependencyInjection.cs
+builder.Services.AddPersistence(builder.Configuration);
+
+builder.Services.AddApiVersioning(o =>
+{
+    // Ange default versionsnummer = 1.0
+    o.DefaultApiVersion = new Microsoft.AspNetCore.Mvc.ApiVersion(1, 0);
+    o.AssumeDefaultVersionWhenUnspecified = true;
+    o.ReportApiVersions = true;
+});
 
 var app = builder.Build();
 
